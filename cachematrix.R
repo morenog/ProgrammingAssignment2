9bda9a48620f875b8edcc3777f97f3589172b519
morenog <gabriel.moreno.au@gmail.com>
2014-11-19 10:39:56 +0800

    Submit for marking

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+###############################################################################
+## File Description
+## 
+## This fie contains two functions that are used to create a special object 
+## (list) that stores a matrix and cache's its inverse.
+##
+###############################################################################
 
-## Write a short comment describing this function
-
+###############################################################################
+## Function Comments
+##
+## makeCacheMatrix creates a list containing a function to:
+## - set the value of the matrix
+## - get the value of the matrix
+## - set the value of the inverse matrix
+## - get the value of the inverse matrix
+##
+###############################################################################
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+###############################################################################
+## Function Comments
+##
+## cacheSolve calculates the inverse of the special "matrix" created with the 
+## makeCacheMatrix function. However, it first checks to see if the inverse 
+## has already been calculated. If so, it gets the inverse from the cache and
+## skips the computation. Otherwise, it calculates the inverse of the matrix
+## and sets the value of the inverse in the cache via the setinv function.
+###############################################################################
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
